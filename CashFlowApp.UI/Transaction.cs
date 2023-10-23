eb6d07ac44fd4bd5fe6a805b055e79d8879c9f46
Andrey Michaely <Andrey@soft-pro.co.il>
2023-10-23 16:06:25 +0300

    initial comit

---
+++ CashFlowApp.UI/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlowApp.UI
+{
+    public class Transaction
+    {
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+    }
+}
